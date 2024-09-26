61df36a09b83e5fb54470285854658e03621886d
sodma <aaa@aaa.com>
2024-09-26 11:50:56 +0900

    출력기능배움

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -1,5 +1,10 @@
 package day01;
 
 public class ExClass01 {
-
+public static void main(String[] args) {
+//syso + ctrl + space 자동완성
+	// sysout는 괄호의 내용을 출력하는 용도
+	System.out.println(100);
+System.out.println( "안녕하세요" );
+}
 }
